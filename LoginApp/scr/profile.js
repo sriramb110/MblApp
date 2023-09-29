018c431adef73b1c5b166ecee11306d216983524
sriramb110 <sriramb110@gmail.com>
2023-09-29 11:42:12 +0530

    install package

---
+++ LoginApp/scr/profile.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const ProfileScreen = () => {};
+
+export default ProfileScreen;
---
+++ LoginApp/scr/signup.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Signupscreen = () => {};
+
+export default Signupscreen;
